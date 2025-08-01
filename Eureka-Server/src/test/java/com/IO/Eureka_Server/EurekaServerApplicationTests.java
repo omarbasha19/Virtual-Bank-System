0c471da32b93441e7f2986f208f8f426beaa2617
Omar Khaled <obasha998@gmail.com>
2025-08-01 07:06:31 +0300

    Add files via upload

---
+++ Eureka-Server/src/test/java/com/IO/Eureka_Server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IO.Eureka_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
